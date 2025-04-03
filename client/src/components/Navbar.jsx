a0031e99240131c019046bb2127c370d9c9c96eb
Dipali2377 <dipalim680@gmail.com>
2025-04-03 15:43:19 +0530

    Navigation bar structure

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,3 +1,23 @@
+import logo from "../assets/logo.svg";
+import { Link } from "react-router-dom";
+import "../styles/Navbar/Navbar.css";
+
 const Navbar = () => {
-  return <div></div>;
+  return (
+    <>
+      <div className="navbar">
+        <div className="logo-div">
+          <img src={logo} alt="" className="logo-img" />
+        </div>
+
+        <div className="menu-div">
+          <Link className="menu">Services</Link>
+          <Link className="menu">SignUp/Login</Link>
+          <button className="tasker-btn">Become a Tasker</button>
+        </div>
+      </div>
+    </>
+  );
 };
+
+export default Navbar;
